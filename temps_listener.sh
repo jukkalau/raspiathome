010845ae1f2b43796e2672e6429b3386440ca1ff
Jukka Laukkanen <jukkalau@gmail.com>
2013-08-13 21:27:14 +0300

    Initial commit for raspiathome
    Raspiathome repo contains collection of scripts to
    read and present temperature values from XRF sensors.
    
    Signed-off-by: Jukka Laukkanen <jukkalau@gmail.com>

---
+++ temps_listener.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# script to check that temps.py is still running
+ps -ef | grep -v grep | grep temps.py
+# if not found - equals to 1, start it
+if [ $? -eq 1 ]
+then
+python ~/raspiathome/temps.py
+#ugly hardcoded path to repo home
+else
+echo "eq 0 - temps.py found - all ok"
+fi
