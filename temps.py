512c7e7da0aba1c2035e7338bb7823aac8e385ae
Jukka Laukkanen <jukkalau@gmail.com>
2014-03-16 20:08:41 +0200

    added initial support for humidity sensor
    tweaked error-recovery hack to get around garbled
    llap messages
    
    Signed-off-by: Jukka Laukkanen <jukkalau@gmail.com>

--- temps.py
+++ temps.py
@@ -27,13 +27,13 @@ my_logger.addHandler(handler)
 parser = ConfigParser.SafeConfigParser()
 parser.read('config.ini')
 
-err = 0 #error counter, hackish way to recover from offsync in xrf communication
 baud = 9600
 port = '/dev/ttyAMA0'
 ser = serial.Serial(port, baud)
 con = sqlite3.connect('temperature.db')
 
 def main():
+    err = 0 # error counter, hackish way to recover from offsync in xrf communication
     while True:
         with con:
             cur = con.cursor()
@@ -41,8 +41,10 @@ def main():
             my_logger.debug(llapMsg)
             #match voltage message
             #matches also negative value aBBTMPA-9.99
-            validator = re.compile("a([A-Z][A-Z])TMPA(( |-)?\d.{1,3}\d)")
-            m = validator.match(llapMsg)
+            validatorTemp = re.compile("a([A-Z][A-Z])TMPA(( |-)?\d.{1,3}\d)")
+            validatorHum = re.compile("a([A-Z][A-Z])HUMA(( |-)?\d.{1,3}\d)")
+            m = validatorTemp.match(llapMsg)
+            mHum = validatorHum.match(llapMsg)
             if m:
                 sqlstring = "insert into temps(sensor,value) values('" + m.group(1)\
                 + "','" + m.group(2) + "')"
@@ -57,6 +59,23 @@ def main():
                 fh.write(sname + ":" + str(round(fixedValue, 2)))
                 fh.close()
                 err = 0
+            elif mHum:  # handle humidity sendor ( by cheating )
+                # todo: add new table to db to hold humidity values
+                # now, only 2nd identifier character being H differentiates temp from
+                # humidity
+                sqlstring = "insert into temps(sensor,value) values('" + mHum.group(1)\
+                + "','" + mHum.group(2) + "')"
+                my_logger.debug(sqlstring)
+                cur.execute(sqlstring)
+                fh = open("www/temp/last_hum" + mHum.group(1) + ".txt", "w+")
+                sname = parser.get(mHum.group(1),"displayName")
+                # fetch calibration multiplier from ini file and use that to
+                # store fixed temp reading to db
+                fixedValue = float(parser.get(mHum.group(1),"calibrationValue")) \
+                * float(mHum.group(2))
+                fh.write(sname + ":" + str(round(fixedValue, 2)))
+                fh.close()
+                err = 0
             # todo: fix voltage reporting to be done in correct sensor file
             elif re.search("BATT", llapMsg):
                 value = llapMsg[7:12]
@@ -67,13 +86,20 @@ def main():
             elif re.search("AWAKE", llapMsg):
                 my_logger.debug("AWAKE message catched")
 	        err = 0
+            elif re.search("STARTED", llapMsg):
+                my_logger.debug("STARTED message catched")
+                err = 0
+            elif re.search("SLEEPING", llapMsg):
+                my_logger.debug("STARTED message catched")
+                err = 0
             else:
-                if err > 10:
+                if err > 13:
                     my_logger.debug("Error count hit, bailing out")
                     return 1
                 my_logger.debug(
-                    "reg-exp did not catch message, emptying serial buffer")
-                null = ser.read(24)
-                err = e + 1
+                    "reg-exp did not catch message, reading extra char from serial buffer")
+                crap = ser.read(1)
+                my_logger.debug(crap)
+                err = err + 1
 
 main()
